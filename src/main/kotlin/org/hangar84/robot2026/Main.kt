62f0724c36adf9356998690c84c5654416f8379d
Zavion Cyboron <z.cyboron07@gmail.com>
2025-10-30 01:12:22 -0600

    refactor: Change Main.kt from Kotlin file to Kotlin Object

--- src/main/kotlin/org/hangar84/robot2026/Main.kt
+++ src/main/kotlin/org/hangar84/robot2026/Main.kt
@@ -1,5 +1,21 @@
 package org.hangar84.robot2026
 
 import edu.wpi.first.wpilibj.RobotBase
+import java.util.function.Supplier
 
-fun main() = RobotBase.startRobot { Robot }
+/**
+ * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
+ * you are doing, do not modify this file except to change the parameter class to the startRobot call.
+ */
+object Main {
+    /**
+     * Main initialization method. Do not perform any initialization here.
+     *
+     *
+     * If you change your main Robot class (name), change the parameter type.
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        RobotBase.startRobot(Supplier { org.hangar84.robot2026.Robot })
+    }
+}
